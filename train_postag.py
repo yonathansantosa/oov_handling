300a27d35d524d04b82c04752430e90e4483436a
yonathansantosa <yonathansantosa@gmail.com>
2019-11-11 14:53:48 +0100

    add save embedding

--- train_postag.py
+++ train_postag.py
@@ -39,6 +39,7 @@ parser.add_argument('--maxepoch', default=30, help='maximum iteration (default=1
 parser.add_argument('--run', default=0, help='starting epoch (default=1000)')
 parser.add_argument('--save', default=False, action='store_true', help='whether to save model or not')
 parser.add_argument('--load', default=False, action='store_true', help='whether to load model or not')
+parser.add_argument('--test', default=False, action='store_true', help='whether to only test the model or not')
 parser.add_argument('--lang', default='en', help='choose which language for word embedding')
 parser.add_argument('--model', default='lstm', help='choose which mimick model')
 parser.add_argument('--lr', default=0.1, help='learning rate')
@@ -223,7 +224,9 @@ validation_loader = DataLoader(dataset, batch_size=val_batch_size,
 postagger = Postagger_adaptive(seq_len, emb_dim, 20, len(dataset.tagset)).to(device)
 
 if args.load:
-    postagger.load_state_dict(torch.load('%s/postag.pth' % (saved_postag_path)))
+    postagger.load_state_dict(torch.load('%s/postagger.pth' % (saved_postag_path)))
+if args.load and args.train_embed:
+    word_embedding.word_embedding.load_state_dict(torch.load('%s/embedding.pth' % (saved_postag_path)))
     
 if not args.freeze and not args.oov_random:
     params = list(postagger.parameters()) + list(model.parameters())
@@ -236,110 +239,113 @@ criterion = nn.NLLLoss()
 step = 0
 
 #* Training
-for epoch in trange(int(args.epoch), max_epoch, total=max_epoch, initial=int(args.epoch)):
-    loss_item = 0.
-    postagger.train()
-    
-    if not (args.freeze or args.oov_random): model.train()
-    if args.train_embed: word_embedding.word_embedding.train()
-    for it, (X, y) in enumerate(train_loader):
-        postagger.zero_grad()
-        if args.freeze or args.oov_random:
-            inputs = Variable(X).to(device)
-            embeddings = word_embedding.word_embedding(inputs)
-        else:
-            words = [word_embedding.idxs2words(x) for x in X]
-            idxs = char_embed.char_sents_split(words, args.model)
-            if args.model == 'lstm':
-                idxs_len = torch.LongTensor([len(data) for data in idxs])
-                generated_embeddings = model.forward((idxs, idxs_len)).view(X.size(0),-1,emb_dim)
-            else: 
-                idxs = nn.utils.rnn.pad_sequence(idxs, batch_first=True)
-                idxs = idxs.unsqueeze(1)
-                inputs = Variable(idxs).to(device) # (batch x channel x seq_len)
-                generated_embeddings = model.forward(inputs).view(X.size(0),-1,emb_dim)
-            
-            mask = (X < original_vocab_len).type(torch.FloatTensor).unsqueeze(2).to(device)
-            words = Variable(X).to(device)
-            pretrained_embeddings = word_embedding.word_embedding(words)
-            embeddings = mask * pretrained_embeddings + (1-mask) * generated_embeddings
-
-        target = Variable(y).to(device)
-        output, loss = postagger.forward(embeddings, target)
+if not args.test:
+    for epoch in trange(int(args.epoch), max_epoch, total=max_epoch, initial=int(args.epoch)):
+        loss_item = 0.
+        postagger.train()
         
-        # Tensorboard
-        loss_item = loss.item()
-        info = {
-            'loss-Train-%s-postag-run%s' % (args.model, args.run) : loss_item,
+        if not (args.freeze or args.oov_random): model.train()
+        if args.train_embed: word_embedding.word_embedding.train()
+        for it, (X, y) in enumerate(train_loader):
+            postagger.zero_grad()
+            if args.freeze or args.oov_random:
+                inputs = Variable(X).to(device)
+                embeddings = word_embedding.word_embedding(inputs)
+            else:
+                words = [word_embedding.idxs2words(x) for x in X]
+                idxs = char_embed.char_sents_split(words, args.model)
+                if args.model == 'lstm':
+                    idxs_len = torch.LongTensor([len(data) for data in idxs])
+                    generated_embeddings = model.forward((idxs, idxs_len)).view(X.size(0),-1,emb_dim)
+                else: 
+                    idxs = nn.utils.rnn.pad_sequence(idxs, batch_first=True)
+                    idxs = idxs.unsqueeze(1)
+                    inputs = Variable(idxs).to(device) # (batch x channel x seq_len)
+                    generated_embeddings = model.forward(inputs).view(X.size(0),-1,emb_dim)
+                
+                mask = (X < original_vocab_len).type(torch.FloatTensor).unsqueeze(2).to(device)
+                words = Variable(X).to(device)
+                pretrained_embeddings = word_embedding.word_embedding(words)
+                embeddings = mask * pretrained_embeddings + (1-mask) * generated_embeddings
+
+            target = Variable(y).to(device)
+            output, loss = postagger.forward(embeddings, target)
+            
+            # Tensorboard
+            loss_item = loss.item()
+            info = {
+                'loss-Train-%s-postag-run%s' % (args.model, args.run) : loss_item,
+            }
+            step += 1
+            if args.run != 0:
+                for tag, value in info.items():
+                    logger.scalar_summary(tag, value, step)
+            
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+        if not args.local:
+            copy_tree(logger_dir, cloud_dir+logger_dir)
+            
+        if not args.quiet: tqdm.write('%d | %.4f ' % (epoch, loss_item))
+
+        #* Validation
+        postagger.eval()
+        if not (args.freeze or args.oov_random): model.eval()
+        if args.train_embed: word_embedding.word_embedding.eval()
+        validation_loss = 0.
+        accuracy = 0.
+        for it, (X, y) in enumerate(validation_loader):
+            if args.freeze or args.oov_random:
+                inputs = Variable(X).to(device)
+                embeddings = word_embedding.word_embedding(inputs)
+            else:
+                words = [word_embedding.idxs2words(x) for x in X]
+                idxs = char_embed.char_sents_split(words, args.model)
+                if args.model == 'lstm':
+                    idxs_len = torch.LongTensor([len(data) for data in idxs])
+                    generated_embeddings = model.forward((idxs, idxs_len)).view(X.size(0),-1,emb_dim)
+                else: 
+                    idxs = nn.utils.rnn.pad_sequence(idxs, batch_first=True)
+                    idxs = idxs.unsqueeze(1)
+                    inputs = Variable(idxs).to(device) # (batch x channel x seq_len)
+                    generated_embeddings = model.forward(inputs).view(X.size(0),-1,emb_dim)
+                mask = (X < original_vocab_len).type(torch.FloatTensor).unsqueeze(2).to(device)
+                pretrained_embeddings = word_embedding.word_embedding(Variable(X).to(device))
+                embeddings = mask * pretrained_embeddings + (1-mask) * generated_embeddings
+                
+            target = Variable(y).to(device)
+            output, validation_loss = postagger.validation(embeddings, target)
+            output_tag = output.view(X.shape[0], seq_len)
+            accuracy += float((output_tag == target).sum())
+
+            if not args.quiet:
+                if it == 0:
+                    tag = output_tag[0]
+                    for i in range(len(X[0])):
+                        word_idx = X[0][i].cpu().numpy()
+                        word = word_embedding.idx2word(word_idx)
+                        tg = dataset.tagset.idx2tag(int(tag[i].cpu()))
+                        tgt = dataset.tagset.idx2tag(int(y[0][i]))
+                        tqdm.write('(%s, %s) => %s' % (word, tgt, tg))
+        accuracy /= (seq_len*len(val_indices))
+        if not args.quiet: tqdm.write('accuracy = %.4f' % accuracy)
+
+        info_val = {
+            'loss-Train-%s-postag-run%s' % (args.model, args.run) : validation_loss
         }
-        step += 1
         if args.run != 0:
-            for tag, value in info.items():
-                logger.scalar_summary(tag, value, step)
-        
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-    if not args.local:
-        copy_tree(logger_dir, cloud_dir+logger_dir)
-        
-    if not args.quiet: tqdm.write('%d | %.4f ' % (epoch, loss_item))
+            for tag, value in info_val.items():
+                logger_val.scalar_summary(tag, validation_loss, step)
 
-    #* Validation
+        if not args.quiet: tqdm.write('val_loss %.4f ' % validation_loss)
+        
+        postagger.train()
+
+    if args.save: 
+        torch.save(postagger.state_dict(), f'{saved_postag_path}/postagger.pth')
+        torch.save(word_embedding.word_embedding.state_dict(), f'{saved_postag_path}/embedding.pth')
     postagger.eval()
-    if not (args.freeze or args.oov_random): model.eval()
-    if args.train_embed: word_embedding.word_embedding.eval()
-    validation_loss = 0.
-    accuracy = 0.
-    for it, (X, y) in enumerate(validation_loader):
-        if args.freeze or args.oov_random:
-            inputs = Variable(X).to(device)
-            embeddings = word_embedding.word_embedding(inputs)
-        else:
-            words = [word_embedding.idxs2words(x) for x in X]
-            idxs = char_embed.char_sents_split(words, args.model)
-            if args.model == 'lstm':
-                idxs_len = torch.LongTensor([len(data) for data in idxs])
-                generated_embeddings = model.forward((idxs, idxs_len)).view(X.size(0),-1,emb_dim)
-            else: 
-                idxs = nn.utils.rnn.pad_sequence(idxs, batch_first=True)
-                idxs = idxs.unsqueeze(1)
-                inputs = Variable(idxs).to(device) # (batch x channel x seq_len)
-                generated_embeddings = model.forward(inputs).view(X.size(0),-1,emb_dim)
-            mask = (X < original_vocab_len).type(torch.FloatTensor).unsqueeze(2).to(device)
-            pretrained_embeddings = word_embedding.word_embedding(Variable(X).to(device))
-            embeddings = mask * pretrained_embeddings + (1-mask) * generated_embeddings
-            
-        target = Variable(y).to(device)
-        output, validation_loss = postagger.validation(embeddings, target)
-        output_tag = output.view(X.shape[0], seq_len)
-        accuracy += float((output_tag == target).sum())
-
-        if not args.quiet:
-            if it == 0:
-                tag = output_tag[0]
-                for i in range(len(X[0])):
-                    word_idx = X[0][i].cpu().numpy()
-                    word = word_embedding.idx2word(word_idx)
-                    tg = dataset.tagset.idx2tag(int(tag[i].cpu()))
-                    tgt = dataset.tagset.idx2tag(int(y[0][i]))
-                    tqdm.write('(%s, %s) => %s' % (word, tgt, tg))
-    accuracy /= (seq_len*len(val_indices))
-    if not args.quiet: tqdm.write('accuracy = %.4f' % accuracy)
-
-    info_val = {
-        'loss-Train-%s-postag-run%s' % (args.model, args.run) : validation_loss
-    }
-    if args.run != 0:
-        for tag, value in info_val.items():
-            logger_val.scalar_summary(tag, validation_loss, step)
-
-    if not args.quiet: tqdm.write('val_loss %.4f ' % validation_loss)
-    
-    postagger.train()
-
-if args.save: torch.save(postagger.state_dict(), f'{saved_postag_path}/postagger.pth')
-postagger.eval()
 if not (args.freeze or args.oov_random): model.eval()
 
 accuracy = 0.
