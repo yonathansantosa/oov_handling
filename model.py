5db43b4fe6665d7d193bc4b2ac3d77fc1caeb67f
yonathansantosa <yonathansantosa@gmail.com>
2021-01-07 23:25:35 +0700

    add new model

--- model.py
+++ model.py
@@ -117,3 +117,108 @@ class mimick_cnn(nn.Module):
         out = self.mlp2(norm_out_cnn)
 
         return out
+
+
+class mimick_cnn_ascii(nn.Module):
+    def __init__(self, embedding, char_emb_dim=300, emb_dim=300, dropout=0.):
+        '''
+        Character Embedding model with binary representation
+        
+        init:
+        -----
+            CNN grams model for mimicking embedding
+            
+            Arguments:
+            embedding = character embedding (nn.Embedding)
+            char_emb_dim = character embedding dimension (int)
+            emb_dim = word embedding dimension (int)
+        '''
+        super(mimick_cnn_ascii, self).__init__()
+        self.embedding = nn.Embedding(embedding.num_embeddings, embedding.embedding_dim, padding_idx=0)
+        self.embedding.weight.data.copy_(embedding.weight.data)
+        self.dropout = nn.Dropout(dropout)
+        # self.num_feature = num_feature
+
+        self.char_size = char_emb_dim
+        kernel_sizes = [10, 7, 5, 2]
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, kernel_sizes[0], kernel_size=(self.char_size, 2), padding=(0,1)),
+            nn.ReLU()
+        )
+        self.pool1 = nn.MaxPool2d((1, 2), ceil_mode=True)
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(1, kernel_sizes[1], (kernel_sizes[0], 2), padding=(0,1) ),
+            nn.ReLU()
+        )
+
+        self.pool2 = nn.MaxPool2d((1, 2), ceil_mode=True)
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(1, kernel_sizes[2], (kernel_sizes[1], 2), padding=(0,1)),
+            nn.ReLU()
+        )
+
+        self.pool3 = nn.MaxPool2d((1, 2), ceil_mode=True)
+
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(1, kernel_sizes[3], (kernel_sizes[2], 2), padding=(0,1)),
+            nn.ReLU()
+        )
+
+        self.FC = nn.Sequential(
+            nn.Linear(3, 300),
+            nn.ReLU(),
+            nn.Linear(300, emb_dim)
+        )
+
+    def forward(self, inputs):
+        '''
+        Forward pass for the model
+
+        input:
+        ------
+            x (torch.FloatTensor) : Inputs with the size of N x C x H x W
+                N : number of data/minibatch size
+                C : number of channel(s), must be size of 1
+                H : height, must be equal with char_size
+                W : width
+            
+        output:
+        -------
+            out (torch.FloatTensor) : output with the size of N x E
+                N : number of data/minibatch size
+                E : word embedding size
+        '''
+        x = self.dropout(self.embedding(inputs).float())
+
+        if x.shape[3] != self.char_size:
+            raise Exception('Character size of inputs and model is different')
+        
+        # N x 1 x emb_dim x L
+        c1 = self.conv1(x)
+        c1 = torch.reshape(c1, (c1.shape[0], 1, -1, c1.shape[3]))
+        p1 = self.pool1(c1)
+
+
+        # N x 1 x 30 x L1: ceil((L-1)/2)
+        c2 = self.conv2(p1)
+        c2 = torch.reshape(c2, (c2.shape[0], 1, -1, c2.shape[3]))
+        p2 = self.pool2(c2)
+
+
+        # N x 1 x 30 x L1: ceil(((L-1)/2) - 1)/2)
+        c3 = self.conv3(p2)
+        c3 = torch.reshape(c3, (c3.shape[0], 1, -1, c3.shape[3]))
+        p3 = self.pool3(c3)
+
+        c4 = self.conv4(p3)
+        c4 = torch.reshape(c4, (c4.shape[0], 1, -1, c4.shape[3]))
+        p4 = F.max_pool2d(c4, kernel_size=(c4.shape[2], 1), ceil_mode=True)
+
+        p4 = torch.reshape(p4, (p4.shape[0], -1))
+
+        out = self.FC(p4)
+
+        return out
