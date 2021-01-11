99be312c7eef54cacd734d8fe2ea50a5100dd5f0
yonathansantosa <yonathansantosa@gmail.com>
2021-01-11 10:03:11 +0700

    adding onehot embedding

--- charembedding.py
+++ charembedding.py
@@ -1,10 +1,11 @@
 import numpy as np
 import torch
+from torch._C import dtype
 import torch.nn as nn
 import torch.nn.functional as F
 
 class Char_embedding:
-    def __init__(self, char_emb_dim=300, char_max_len=15, random=False, asc=False, device='cuda', freeze=False):
+    def __init__(self, char_emb_dim=300, char_max_len=15, random=False, embed_type='random', device='cuda', freeze=False):
         super(Char_embedding, self).__init__()
         '''
         Initializing character embedding
@@ -13,16 +14,16 @@ class Char_embedding:
         ascii = mutually exclusive with random
         '''
         self.char_max_len = char_max_len
-        self.asc = asc
+        self.embedding_type = embed_type
         self.embedding_folder = 'embeddings/char_embedding'
-        if random and not self.asc:
+        if self.embedding_type=='random':
             torch.manual_seed(5)
             table = np.transpose(np.loadtxt(f'{self.embedding_folder}/glove.840B.300d-char.txt', dtype=str, delimiter=' ', comments='##'))
             self.weight_char = np.transpose(table[1:].astype(np.float))
             self.char = np.transpose(table[0])
             self.embed = nn.Embedding(len(self.char), char_emb_dim).to(device)
             self.embed.weight[1] = torch.zeros(char_emb_dim)
-        elif self.asc:
+        elif self.embedding_type=='ascii':
             table = np.transpose(np.loadtxt(f'{self.embedding_folder}/ascii.embedding.txt', dtype=str, delimiter=' ', comments='##'))
             self.char = np.transpose(table[0])
             self.weight_char = np.transpose(table[1:].astype(np.float))
@@ -31,6 +32,16 @@ class Char_embedding:
             
             self.embed = nn.Embedding.from_pretrained(self.weight_char, freeze=freeze)
             char_emb_dim = 8
+        elif self.embedding_type=='onehot':
+            chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890,.!@#$%^&*|()_+-=[]<>?/\\;\'"\t\n '
+            self.ix2char = [c for c in chars]
+            self.ix2char = ['<unk>', '<eow>', '<sow>'] + self.ix2char
+            ix = np.arange(len(self.ix2char))
+            self.char2ix = dict(zip(self.ix2char,ix))
+            self.char_size = len(self.ix2char)
+            self.weight_char = torch.transpose(nn.functional.one_hot(torch.arange(len(self.ix2char), dtype=torch.long), num_classes = len(self.ix2char)+1), 1,0)
+            self.embed = nn.Embedding.from_pretrained(self.weight_char.type(torch.float)[:-1], freeze=True)
+            None
         else:
             table = np.transpose(np.loadtxt(f'{self.embedding_folder}/glove.840B.300d-char.txt', dtype=str, delimiter=' ', comments='##'))
             self.char = np.transpose(table[0])
@@ -68,6 +79,7 @@ class Char_embedding:
                 c_idx = c_idx + [self.char2idx['<pad>']] * 7
             char_data += [torch.LongTensor(c_idx)]
         return char_data
+
     def char_sents_split(self, sentences, model_name='lstm', dropout=0.):
         '''
         Splitting character of a sentences then converting it
